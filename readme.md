d055df52f7ac13aefd3c54be3cc94c6d39971d37
Tim Schaub <tim.schaub@gmail.com>
2015-08-21 19:45:26 -0600

    Setup

---
+++ readme.md
@@ -0,0 +1,5 @@
+# `planetbuf`
+
+A small service that transforms Planet API responses so payloads are encoded as protocol buffers instead of JSON.
+
+This currently only transforms Scenes API responses.
---
+++ serve.js
@@ -0,0 +1,148 @@
+var http = require('http');
+var https = require('https');
+var url = require('url');
+
+var Pbf = require('pbf');
+var api = require('planet-client');
+var geobuf = require('geobuf-published');
+
+var corsHeaders = {
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Headers': 'Accept, Authorization, Content-Type',
+  'Access-Control-Allow-Methods': 'DELETE, GET, OPTIONS, POST, PUT',
+  'Access-Control-Max-Age': 86400,
+  'Allow': 'HEAD, GET, POST, OPTIONS',
+  'Strict-Transport-Security': 'max-age=31536000',
+  'Vary': 'Origin'
+};
+
+/**
+ * Handler for all OPTIONS requests.
+ * @param {http.IncomingMessage} req The request.
+ * @param {http.ServerResponse} res The response.
+ */
+function optionsHandler(req, res) {
+  var headers = assign({
+    'Access-Control-Allow-Origin': req.headers.origin,
+    'Content-Length': 0
+  }, corsHeaders);
+
+  res.writeHead(200, headers);
+  res.end();
+}
+
+/**
+ * Proxy handler for all non-scenes requests.
+ * @param {http.IncomingMessage} req The request.
+ * @param {http.ServerResponse} res The response.
+ */
+function proxyHandler(req, res) {
+  var headers = req.headers;
+  headers.host = 'api.planet.com';
+
+  var options = {
+    hostname: 'api.planet.com',
+    method: req.method,
+    path: url.parse(req.url).path,
+    headers: headers
+  };
+
+  var proxyClient = https.request(options, function(serverRes) {
+    res.writeHead(serverRes.statusCode, serverRes.headers);
+    serverRes.pipe(res);
+  });
+
+  proxyClient.on('error', function(proxyErr) {
+    process.stderr.write(proxyErr.message + '\n');
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.end('Proxy Error');
+  });
+
+  req.pipe(proxyClient);
+}
+
+/**
+ * Transforms GeoJSON responses from the API to geobuf responses for the client.
+ * @param {http.IncomingMessage} req The request.
+ * @param {http.ServerResponse} res The response.
+ * @param {api.Page} page A page of scenes.
+ */
+function scenesResponse(req, res, page) {
+  var buffer = geobuf.encode(page.data, new Pbf());
+
+  var headers = assign({
+    'Content-Type': 'application/x-protobuf',
+    'Content-Length': buffer.length,
+    'Access-Control-Allow-Origin': req.headers.origin
+  }, corsHeaders);
+
+  res.writeHead(200, headers);
+  res.end(buffer);
+}
+
+/**
+ * Handles errors in fetching scenes from the API.
+ * @param {http.IncomingMessage} req The request.
+ * @param {http.ServerResponse} res The response.
+ * @param {Error} err The API error.
+ */
+function scenesError(req, res, err) {
+  var errResponse = err.response;
+  if (errResponse) {
+    res.writeHead(errResponse.statusCode, errResponse.headers);
+    if (err.body) {
+      res.write(JSON.stringify(err.body));
+    }
+    res.end();
+  } else {
+    process.stderr.write(err.message + '\n');
+    res.writeHead(500);
+    res.end('Unknown error');
+  }
+}
+
+/**
+ * Handler for scenes requests.
+ * @param {http.IncomingMessage} req The request.
+ * @param {http.ServerResponse} res The response.
+ */
+function scenesHandler(req, res) {
+  api.auth.setKey(req.headers.authorization.split(' ')[1])
+
+  var parts = url.parse(req.url, true);
+  var query = query.query;
+  query.type = parts.pathname.split('/')[3];
+
+  api.scenes.search(query, {augmentLinks: false})
+    .then(scenesResponse.bind(null, req, res))
+    .catch(scenesError.bind(null, req, res))
+}
+
+/**
+ * Top level request handler.
+ * @param {http.IncomingMessage} req The request.
+ * @param {http.ServerResponse} res The response.
+ */
+function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    return optionsHandler(req, res);
+  }
+
+  if (url.parse(req.url).pathname.indexOf('/v0/scenes/') === 0) {
+    return scenesHandler(req, res);
+  }
+
+  return proxyHandler(req, res);
+}
+
+function assign(target, source) {
+  for (var key in source) {
+    target[key] = source[key];
+  }
+  return target;
+}
+
+if (require.main === module) {
+  http.createServer(handler).listen(3003);
+  process.stdout.write('Server started: http://localhost:3000/\n');
+}
